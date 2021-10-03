300729bdcf3db399caffdf3dc8317f6d07779b7b
JuSoft <70066080+jusoftdev@users.noreply.github.com>
2021-10-03 15:15:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# commited
+
+Yeah, as you see this project has a looooot of commits
---
+++ index.ts
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+
+const times = 188135;
+
+for (let i = times; i < 69_000_000 + 1; i++) {
+  console.log(`Commit ${i} (${new Date().toLocaleTimeString()}):`);
+
+  const now = Date.now();
+
+  execSync(`git commit --allow-empty -m "${i}"`);
+
+  console.log(`Done in ${Date.now() - now}ms`);
+}
+
+console.log(`Finished commiting ${times.toLocaleString()} times!`);
+
+// This is made by https://github.com/cursorsdottsx/committed
